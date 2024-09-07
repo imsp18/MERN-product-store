ea419110a43c422fddba43e3adda1c1804d7cddd
imsp18 <shivangparmar537@gmail.com>
2024-09-07 18:57:59 +0530

    feat: Add endpoint for creating new products

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.post("/products", async (req, res) => {
+  const product = req.body; // user will send this data
 
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please Provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ sucess: true, data: newProduct });
+  } catch (error) {
+    console.log("Error in Create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
 
 app.listen(5000, () => {
   connectDB();
