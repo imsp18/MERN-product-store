e6bf593946e9396f40fce9ad006f5c47d04f1ca5
imsp18 <shivangparmar537@gmail.com>
2024-09-08 21:38:52 +0530

    refactor: Update Navbar component layout, styling, and dependencies

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
-import { Container, Flex, Text } from "@chakra-ui/react"
+import { Button, Container, Flex, HStack, Link, Text, useColorMode } from "@chakra-ui/react"
+import { BsPlusSquare } from "react-icons/bs"
 
 const Navbar = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+
     return (
         <Container maxW={"1140px"} px={4}>
             <Flex
@@ -12,8 +15,27 @@ const Navbar = () => {
                     sm: "row"
                 }}
             >
-                <Text></Text>
+                <Text
+                    fontSize={{ base: "22", sm: "28" }}
+                    fontWeight={"bold"}
+                    textTransform={"uppercase"}
+                    textAlign={"center"}
+                    bgGradient={"linear(to-r, cyan.400, blue.500)"}
+                    bgClip={"text"}
+                >
+                    <Link to={"/"}>PRODUCT STORE 🛒</Link>
+                </Text>
 
+                <HStack spacing={2} alignItems={"center"}>
+                    <Link to={"/create"}>
+                        <Button>
+                            <BsPlusSquare fontSize={20} />
+                        </Button>
+                    </Link>
+                    <Button onClick={toggleColorMode}>
+                        {colorMode === 'light' ? '🌙' : '☀️'}
+                    </Button>
+                </HStack>
             </Flex>
         </Container>
     )
