2d58c8acca6a277fd3d6ad2161528c47f88f478f
imsp18 <shivangparmar537@gmail.com>
2024-09-08 18:09:38 +0530

    refactor: Update server.js to use dynamic port for listening

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,12 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
-app.use(express.json()); //allows us to accept JSON data in the req.body
+const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); //allows us to accept JSON data in the req.body
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("server at http://localhost:5000/");
+  console.log("server at http://localhost:" + PORT);
 });
