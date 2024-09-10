7170e1fff955025ecfff6d4309bcd7198e061c13
imsp18 <shivangparmar537@gmail.com>
2024-09-10 11:23:53 +0530

    refactor: Update CreatePage component to use Chakra UI Container, add state for new product, and improve layout and styling

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,4 +1,4 @@
-import { Container } from "@chakra-ui/react"
+import { Box, Button, Container, Heading, Input, useColorModeValue, VStack } from "@chakra-ui/react"
 import { useState } from "react"
 
 const CreatePage = () => {
@@ -6,10 +6,49 @@ const CreatePage = () => {
         name: "",
         price: "",
         image: "",
-    })
+    });
+
+    const handleProduct = () => {
+        console.log(newProduct);
+
+    }
     return (
-        <Container> 
-            CreatePage
+        <Container maxW={"container.sm"}>
+            <VStack
+                spacing={8}>
+                <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+                    Create New Product
+                </Heading>
+
+                <Box
+                    w={"full"}
+                    bg={useColorModeValue("white", "gray.800")}
+                    p={6}
+                    rounded={"lg"}
+                    shadow={"md"}
+                >
+                    <VStack spacing={4}>
+                        <Input placeholder="Product Name"
+                            name="name"
+                            value={newProduct.name}
+                            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+                        />
+                        <Input placeholder="Price"
+                            name="price"
+                            type="number"
+                            value={newProduct.price}
+                            onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+                        />
+                        <Input placeholder="Image URL"
+                            name="image"
+                            value={newProduct.image}
+                            onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
+                        />
+                        <Button colorScheme="blue" onClick={handleProduct} w={"w-full"} >Add a product</Button>
+                    </VStack>
+                    
+                </Box>
+            </VStack>
         </Container>
     )
 }
