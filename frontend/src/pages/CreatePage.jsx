9e7a213712b2f58d83f7a4c9b561efe1a6813871
imsp18 <shivangparmar537@gmail.com>
2024-09-12 23:07:17 +0530

    refactor: Update CreatePage to include toast notifications for product creation

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Heading, Input, useColorModeValue, VStack } from "@chakra-ui/react"
+import { Box, Button, Container, Heading, Input, useColorModeValue, useToast, VStack } from "@chakra-ui/react"
 import { useState } from "react"
 import { useProductStore } from "../store/product";
 
@@ -9,13 +9,29 @@ const CreatePage = () => {
         image: "",
     });
 
+    const toast = useToast()
+
+
     const { createProduct } = useProductStore()
 
     const handleProduct = async () => {
         const { success, message } = await createProduct(newProduct);
-        console.log("Success:", success)
-        console.log("Message:", message)
-
+        if (!success)
+            toast({
+                title: "Error ",
+                description: message,
+                status: "error",
+                isClosable: true
+            })
+        else {
+            toast({
+                title: "Success",
+                description: message,
+                status: "success",
+                isClosable: true
+            })
+        }
+        setNewProduct({ name: "", price: "", image: "" });
     }
     return (
         <Container maxW={"container.sm"}>
