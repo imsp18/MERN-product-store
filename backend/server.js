49080842af87987e0a4a776cd0f0d63792e587a4
imsp18 <shivangparmar537@gmail.com>
2024-09-18 00:15:37 +0530

    refactor: Update server.js to handle production build and deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import { connectDB } from "./config/db.js";
+
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
@@ -8,10 +11,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); //allows us to accept JSON data in the req.body
+const __dirname = path.resolve();
+
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
 app.use("/api/products", productRoutes);
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 app.listen(PORT, () => {
-  connectDB();
-  console.log("server at http://localhost:" + PORT);
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
 });
